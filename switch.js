bcd0e4b8d29c20eb52996aae3855aca5238b2778
Mayuri Gavhane <mayurigavhane1504@gmail.com>
2026-02-12 17:29:01 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,23 @@
+let time=prompt("Enter time");
+switch(time){
+    case ("6"):
+       console.log("Good Morning");
+       
+    break;
+        case ("9"):
+            console.log("Good night");
+            
+        break;
+            case ("7"):
+                console.log("Good Evening");
+                
+            break;
+                case ("12"):
+                    console.log("Good afternoon");
+                    
+                break;
+                    default:
+                    console.log("enter worang time");
+                    
+                
+}
---
+++ traffic.js
@@ -0,0 +1,17 @@
+let trafficlight=prompt("enter the color");
+
+switch(trafficlight){
+    case("red"):
+    console.log("Stop Immediately...");
+    break;
+    case("yellow"):
+    console.log("Prepare to stop...");
+    break;
+    case("green"):
+    console.log("Proceed or continue driving...");
+    break;
+    default:
+        console.log("Invalid traffic light color...");
+ 
+    
+}
