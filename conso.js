871a11b86e711a0a461faa94a2c25c075f8eab5a
Mayuri Gavhane <mayurigavhane1504@gmail.com>
2026-02-20 21:58:20 +0530

    Add files via upload

---
+++ conso.js
@@ -0,0 +1,38 @@
+let z="Mayuri Gavhane"
+console.log(a);
+
+// let b=a.charCodeAt(2)
+// console.log(b);
+
+// let c=a.charAt(2)
+// console.log(c);
+
+// let d=a.startsWith("x ")
+// console.log(d);
+
+// let e=a.endsWith("R")
+// console.log(e);
+
+// let f=a.toLocaleLowerCase()
+// console.log(f);
+
+// let g=a.toLocaleUpperCase()
+// console.log(g);
+
+// let h=a.includes("u")
+// console.log(h);
+
+// let i=a.repeat(10)
+// console.log(i);
+
+// let j=a.replace("Fortune","cravita")
+// console.log(j);
+
+// let k=a.concat("pune")
+// console.log(k);
+
+// let l=a.split("cloud")
+// console.log(1);
+
+// let m=a.slice(3)
+// console.log(m);
---
+++ dom.js
@@ -0,0 +1,15 @@
+let a= document.getElementById("h11")
+a.style.backgroundColor = "red"
+a.style.textAlign = "center"
+a.style.fontSize = "30px"
+
+console.log(a);
+
+let b= document.getElementsByClassName("p11")
+for(let i=0; i<=b.length;i++){
+    b[i].style.color = "blue"
+    b[1].style.color = "pink"
+    b[2].style.color = "green"
+}
+console.log(b);
+
